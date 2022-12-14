5641deea4b01bb73e3ff4f21a30f08dcdac16f5c
ernesto.maria <ernestomaria93@gmail.com>
2022-12-14 11:05:12 +0100

    add class validator

--- src/createNotificationBody.ts
+++ src/createNotificationBody.ts
@@ -1,10 +1,14 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
 
 export class CreateNotificationBody {
   @IsNotEmpty()
+  @IsUUID()
   recipientId: string;
 
+  @IsNotEmpty()
+  @Length(5, 500)
   content: string;
 
+  @IsNotEmpty()
   category: string;
 }
